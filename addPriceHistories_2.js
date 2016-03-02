e0fac352a69f3fcfc22276638c597d551c157a48
Anna Rogers <2muddybarefeet@gmail.com>
2016-03-01 17:52:15 -0800

    Initial commit

---
+++ addPriceHistories_2.js
@@ -0,0 +1,87 @@
+//take the data from abercrombieData.formatted and augment with new data from v1/offers
+
+var fs = require('fs');
+var LineByLineReader = require('line-by-line');
+var keys = require('./keys.js');
+var moment = require('moment');
+
+//get the arguments passed in from command line
+var args = process.argv.slice(2);
+
+//auth for Semantics3 api
+var api_key = keys.api_key;
+var api_secret = keys.api_secret;
+var sem3 = require('semantics3-node')(api_key,api_secret);
+
+var lr = new LineByLineReader(args[0]);
+var outputFile = fs.createWriteStream(args[1]);
+
+//handle errors on writing newStream
+outputFile.on('error', function(err) {
+  console.log('error in writing file', err);
+});
+
+
+//variable to be used when inserting comas between objects in the outputFile
+var separator = "";
+
+//to insert into the outputFile so that the data converted to csv is in a friendly format
+outputFile.write("[");
+
+
+lr.on('error', function (err) {
+  console.log('error in line by line module', err);
+});
+
+
+lr.on('line', function (line) {
+  // pause emitting of lines do do async function
+  lr.pause();
+
+  line = JSON.parse(line);
+
+  sem3.products.offers_field( "sem3_id", line.sem3_id.toString() );
+
+  sem3.products.get_offers(/*endpoint, jsonStr , method,*/
+
+
+    function(err, offers) {
+
+      if (err) {
+         console.log("Couldn't execute request: run_query", err);
+         return;
+      }
+
+      //take the data from the api and add to the current object
+      JSON.parse(offers).results.forEach(function (price) {
+        var date = moment(new Date(price.lastrecorded_at * 1000)).format('L');
+        line[date] = price.price;
+      });
+
+      //write the new line to the outputFile
+      outputFile.write(separator + JSON.stringify(line, null, 2));
+      if (!separator) {
+        separator = ",";
+      }
+
+      lr.resume();
+    }
+
+  );
+
+});
+
+
+//all the lines of the inputFile have been run
+lr.on('end', function () {
+
+  console.log('stream done');
+  outputFile.write("]");
+  outputFile.end();
+
+});
+
+
+/*
+node addPriceHistories_2.js /Users/anna/Desktop/sem3Project_abercrombie/abercrombieData.formatted /Users/anna/Desktop/sem3Project_abercrombie/abercrombieData.formatted.priced
+*/
---
+++ pullAndFilterData_1.js
@@ -0,0 +1,92 @@
+//Page to pull the data from the API and filter it to contain:
+//product name and product category
+// sem3_id
+// url
+
+//modules used
+var moment = require('moment');
+var fs = require('fs');
+var keys = require('./keys.js');
+
+//get the arguments passed in from command line
+var args = process.argv.slice(2);
+
+//auth for Semantics3 api
+var api_key = keys.api_key;
+var api_secret = keys.api_secret;
+var sem3 = require('semantics3-node')(api_key,api_secret);
+
+//Filter dates
+var startDateFilterFrom = moment(new Date(args[0]));
+var endDateFilterFrom = moment(new Date(args[1]));
+
+var outputFile = fs.createWriteStream(args[2]);
+
+var count = 0;
+
+//error handling for file writing
+outputFile.on('error', function(err) {
+  console.log('err in streaming', err);
+});
+
+
+var getData = function (err, products) {
+  console.log('COUNT', count);
+  if (err) {
+     console.log("Couldn't execute request: iterate_products");
+     return;
+  }
+
+  var filteredByDate = JSON.parse(products).results.filter(function (product) {
+    //filter for the products updates between Aug 1st and Aug 31st 2015
+    if (moment(new Date(product.updated_at * 1000)).isBetween(startDateFilterFrom, endDateFilterFrom)) {
+      return product;
+    }
+  });
+  
+  if (filteredByDate.length > 0) {
+    filteredByDate.map(function (product) {
+      return {
+        sem3_id: product.sem3_id,
+        name: product.name,
+        category: product.category,
+        updated_at: moment(new Date(product.updated_at * 1000)).format('L'),
+        url: product.sitedetails.url
+      };
+
+    }).forEach(function (product) {
+
+      count += 1;
+      outputFile.write(JSON.stringify(product)+ '\n'); //stringify means each object will go on one line
+
+    });
+
+  }
+
+  if (count < 1000) {//------------>?! QUERY
+
+    sem3.products.iterate_products(getData);
+
+  } else {
+
+    console.log('FINISHED');
+    outputFile.end();
+    return;
+
+  }
+
+  console.log("Successfully retrieved next page of products");
+
+};
+
+
+
+sem3.products.products_field( "site", "abercrombie.com");
+sem3.products.products_field( "isactive", 1 );
+
+sem3.products.get_products(getData);
+
+/*
+node pullAndFilterData_1.js '2015/08/01' '2015/08/31' /Users/anna/Desktop/sem3Project_abercrombie/abercrombieData.formatted
+*/
+
