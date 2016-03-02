aeb16ac5edcaf5a81e0ae7e4d16b8f6f3c794b9b
Anna Rogers <2muddybarefeet@gmail.com>
2016-03-02 08:25:30 -0800

    Update methods to pull data from the api so not inner in inner functions

--- pullAndFilterData_1.js
+++ pullAndFilterData_1.js
@@ -1,7 +1,17 @@
 //Page to pull the data from the API and filter it to contain:
-//product name and product category
-// sem3_id
-// url
+
+// sem3_id, -
+// ------------------------------------------upc
+// sku -
+// name -
+// cat_id -
+// crumb -
+// price -
+// timestamp - 
+// description -
+// specifications fields (color, size, etc, in individual columns) weight brand model color size
+// image url - one only currently
+// offer url - ?
 
 //modules used
 var moment = require('moment');
@@ -20,9 +30,11 @@ var sem3 = require('semantics3-node')(api_key,api_secret);
 var startDateFilterFrom = moment(new Date(args[0]));
 var endDateFilterFrom = moment(new Date(args[1]));
 
-var outputFile = fs.createWriteStream(args[2]);
+var startOffset = args[3];
+var outputFile = fs.createWriteStream(args[2] + (startOffset || ''));
 
 var count = 0;
+var offset = startOffset || 0;
 
 //error handling for file writing
 outputFile.on('error', function(err) {
@@ -30,17 +42,56 @@ outputFile.on('error', function(err) {
 });
 
 
-var getData = function (err, products) {
-  // console.log('COUNT', count);
-  if (err) {
-     console.log("Couldn't execute request: iterate_products");
-     return;
+var isWorking = false;
+var didThrowError = false;
+var pageNum = 0;
+
+var doWork = function(){
+  
+  if(isWorking){
+    return;
   }
 
+  isWorking = true;
+
+  if(count < 1000){
+    console.log('Page# ' + pageNum + ' Offset: ' + offset);
+    offset+=10;
+    sem3.products.clear();
+    sem3.products.products_field( "site", "abercrombie.com");
+    sem3.products.products_field( "isactive", 1 );
+    sem3.products.products_field( "offset", offset);
+    sem3.products.get_products(getData);
+  }else{
+    outputFile.end();
+    console.log('FINISHED');
+  }
+
+};
+
+sem3.products.products_field( "site", "abercrombie.com");
+sem3.products.products_field( "isactive", 1 );
+isWorking = true;
+sem3.products.get_products(getData);
+
+setInterval(doWork, 10);
+
+function getData (err, products) {
+
+  pageNum++;
+
+  if (err) {
+    // console.log(err);
+    // console.log("Couldn't execute request: iterate_products");
+    // console.log('Updating offset');
+    // offset += 200;
+    // didThrowError = true;
+    // isWorking = false;
+    // return;
+  }
 
   var filteredByDate = JSON.parse(products).results.filter(function (product) {
     //filter for the products updates between Aug 1st and Aug 31st 2015
-    console.log( 'new date on this data', moment(new Date(product.updated_at * 1000)).format('MM/DD/YYYY') );
     if (moment(new Date(product.updated_at * 1000)).isBetween(startDateFilterFrom, endDateFilterFrom)) {
       return product;
     }
@@ -51,42 +102,28 @@ var getData = function (err, products) {
       return {
         sem3_id: product.sem3_id,
         name: product.name,
+        // description: product.description,
+        // cat_id: product.cat_id,
         category: product.category,
-        updated_at: moment(new Date(product.updated_at * 1000)).format('L'),
-        url: product.sitedetails.url
+        // sku: product.sku,
+        // updated_at: moment(new Date(product.updated_at * 1000)).format('L'),
+        // crumb: product.crumb,
+        url: product.sitedetails[0].url
       };
 
     }).forEach(function (product) {
 
       count += 1;
+      console.log('COUNT', count);
       outputFile.write(JSON.stringify(product)+ '\n'); //stringify means each object will go on one line
 
     });
 
   }
 
-  if (count < 1000) { 
+  isWorking = false;
 
-    sem3.products.iterate_products(getData);
-
-  } else {
-
-    console.log('FINISHED');
-    outputFile.end();
-    return;
-
-  }
-
-  console.log("Successfully retrieved next page of products");
-
-};
-
-
-
-sem3.products.products_field( "site", "abercrombie.com");
-sem3.products.products_field( "isactive", 1 );
-
-sem3.products.get_products(getData);
+}
 
 /*
 node pullAndFilterData_1.js '2015/08/01' '2015/08/31' /Users/anna/Desktop/sem3Project_abercrombie/abercrombieData.formatted
